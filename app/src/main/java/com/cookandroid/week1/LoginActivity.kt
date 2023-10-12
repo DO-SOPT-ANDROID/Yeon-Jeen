ca90f373a1932abe75c2d1be54f373f29261c813
Lee yeon Jeen <jeendev2019@gmail.com>
2023-10-12 13:55:16 +0900

    FEAT LoginActivity

--- app/src/main/java/com/cookandroid/week1/LoginActivity.kt
+++ app/src/main/java/com/cookandroid/week1/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.cookandroid.week1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity;
 import com.cookandroid.week1.databinding.ActivityLoginBinding
 
@@ -12,16 +13,41 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val view = binding.root
+        setContentView(view)
 
         binding.signbtn1.setOnClickListener {
-
             val intent = Intent(this, SignUpActivity::class.java)
-
             startActivity(intent)
         }
 
+        binding.logbtn1.setOnClickListener {
+            val enteredId = binding.eID1.text.toString()
+            val enteredPassword = binding.ePS1.text.toString()
+
+
+            if (checkLogin(enteredId, enteredPassword)) {
+
+                Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+
+                Toast.makeText(this, "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
+    private fun checkLogin(enteredId: String, enteredPassword: String): Boolean {
+
+        val registeredId = intent.getStringExtra("id")
+        val registeredPassword = intent.getStringExtra("password")
+
+        return enteredId == registeredId && enteredPassword == registeredPassword
+    }
 
 }
+
+
+
