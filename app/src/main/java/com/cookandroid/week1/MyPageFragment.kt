a1c9bc88449a40d8c7fcf437a44bff18caee93f1
Lee yeon Jeen <jeendev2019@gmail.com>
2023-11-10 00:09:26 +0900

    [feat] 스피너 mbti 추가

--- app/src/main/java/com/cookandroid/week1/MyPageFragment.kt
+++ app/src/main/java/com/cookandroid/week1/MyPageFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.cookandroid.week1.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
     private lateinit var binding: FragmentMyPageBinding
+    private lateinit var mbtiSpinner: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +28,19 @@ class MyPageFragment : Fragment() {
         binding.tvMypageEditid.text = id
         binding.tvMypageEditadress.text = address
 
+        mbtiSpinner = view.findViewById(R.id.sp_mypage_mbti)
+        val yearAdapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.mbti_type,
+            android.R.layout.simple_spinner_item
+        )
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        mbtiSpinner.adapter = yearAdapter
+
         return view
     }
+
+
+
+
 }
